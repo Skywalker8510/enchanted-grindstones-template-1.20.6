b87042eab02fbfdade7347b20e616f0e8e9f2546
Skywalker8510 <91810623+Skywalker8510@users.noreply.github.com>
2024-05-26 22:51:02 -0500

    Initial commit

---
+++ src/main/java/net/skywalker8510/enchantedgrindstones/EnchantedGrindstonesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.skywalker8510.enchantedgrindstones;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnchantedGrindstonesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
