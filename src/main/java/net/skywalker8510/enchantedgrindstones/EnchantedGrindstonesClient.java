6249bfa29ce460619c0bee0a34da7485110ab705
Skywalker8510 <91810623+Skywalker8510@users.noreply.github.com>
2024-06-02 09:06:20 -0500

    workspace set up

---
+++ src/main/java/net/skywalker8510/enchantedgrindstones/EnchantedGrindstonesClient.java
@@ -0,0 +1,11 @@
+package net.skywalker8510.enchantedgrindstones;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnchantedGrindstonesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
